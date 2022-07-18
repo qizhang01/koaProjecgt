9b4b2c57716b40355b7ec3a1edd8448cf8578bb2
qizhang01 <zhishui120@sina.com>
2022-07-18 22:33:36 +0800

    权限认证模块

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,114 @@
 const router = require('koa-router')()
+const connection = require('../public/javascripts/mysql.js')
+router.prefix('/api/users')
 
-router.prefix('/users')
+const tableDesc = "authlist(userno,name,password,roles)"
+//查询所有用户信息
+router.get('/allusers', async (ctx, next)=> {
+  const  sql = 'SELECT * FROM authlist'
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
+  ctx.type =  'json'
+  ctx.body = {
+    code : 200,
+    msg : '',
+    data : res
+  }
+})
+//添加一个账户
+router.post('/addaccount', async (ctx, next) =>{
+  const {userno,name,password, roles} = ctx.request.body
+  const sql = `INSERT INTO ${tableDesc} VALUES("${userno}","${name}","${password}","${roles}")`
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+  ctx.type =  'json'
+  ctx.body = {
+    code : 200,
+    msg : '提交成功',
+  }
+})
+//更新或者重置密码
+router.post('/updatepassword', async (ctx, next) =>{
+  const {password,id} = ctx.request.body  
+  console.log(id)
+  const sql = `update authlist set password="${password}" where id="${id}"`
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
+
+  ctx.type =  'json'
+  ctx.body = {
+    code : 200,
+    msg : '更新密码成功',
+  }
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
+//更新权限
+router.post('/updateroles', async(ctx, next)=> {
+  const {roles, id} = ctx.request.body  
+  console.log(roles)
+  const sql = `update authlist set roles="${roles}" where id="${id}"`
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
 
+  ctx.type =  'json'
+  ctx.body = {
+    code : 200,
+    msg : '更新权限成功',
+  }
+})
+//停用账户
+router.post('/enduse', async(ctx, next)=> {
+  const {ifopen, id} = ctx.request.body  
+  const status = ifopen==1? 0: 1
+  const sql = `update authlist set ifopen="${status}" where id="${id}"`
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
+
+  ctx.type =  'json'
+  ctx.body = {
+    code : 200,
+    msg : '操作成功',
+  }
+})
 module.exports = router
