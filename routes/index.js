95678d29f893f348469db51cd10c1ba284eeab89
qizhang01 <zhishui120@sina.com>
2022-07-18 13:32:43 +0800

    实现启用和删除接口

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')()
 const connection = require('../public/javascripts/mysql.js')
 router.prefix('/api')
-const tableDesc = 'productList(name,goods_type,goods_no,model_type,price,unit,ifopen,goods_prod_address,buy_date,buy_number,store_house,delivery_address,ifdelete)'
+const tableDesc = 'productlist(name,goods_type,goods_no,model_type,price,unit,ifopen,goods_prod_address,buy_date,buy_number,store_house,delivery_address,ifdelete)'
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
@@ -11,7 +11,7 @@ router.get('/', async (ctx, next) => {
 
 router.get('/productlist', async (ctx, next) => {
    
-  var  sql = 'SELECT * FROM productlist';
+  var  sql = 'SELECT * FROM productlist where ifdelete != 1';
 
   let res = await new Promise((resolve,reject)=>{
     connection.query(sql,function (err, result) {
@@ -83,6 +83,80 @@ router.post('/addExcelImportProduct', async (ctx, next) => {
     code : 200,
     msg : '',
     data : {}
+  }
+})
+
+router.post('/login', async (ctx, next)=>{
+  const {username, password} = ctx.request.body
+  
+  const sql = `select name from authlist where name="${username}" and password="${password}"`
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
+
+  ctx.type =  'json'
+  if(result.length>0){
+    ctx.body = {
+      code : 200,
+      msg : '',
+    }
+  }else{
+    ctx.body = {
+      code : 200,
+      msg : '用户名或者密码错误',
+    }
+  }
+
+})
+
+router.post('/delete', async (ctx, next)=>{
+  const {id} = ctx.request.body  
+  console.log(id)
+  const sql = `update productlist set ifdelete=1 where id="${id}"`
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
+
+  ctx.type =  'json'
+  ctx.body = {
+    code : 200,
+    msg : '删除成功',
+  }
+})
+
+router.post('/startorend', async (ctx, next)=>{
+  const {id,ifopen} = ctx.request.body 
+  const modify = ifopen==1?0:1 
+  const sql = `update productlist set ifopen=${modify} where id="${id}"`
+  let res = await new Promise((resolve,reject)=>{
+    connection.query(sql,function (err, result) {
+      if(err){
+        console.log(err.message);
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    });
+  })
+
+  ctx.type =  'json'
+  ctx.body = {
+    code : 200,
+    msg : '修改成功',
   }
 })
 
